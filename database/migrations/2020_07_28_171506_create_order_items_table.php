2671f25ad90cd3061c4736eecca05936791b31cf
yota.noguchi <y.nogu2045@icloud.com>
2020-07-28 12:16:26 +0000

    make model

---
+++ database/migrations/2020_07_28_171506_create_order_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('number_of_purchase');
+            $table->timestamps();
+            
+            // 外部キー制約
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
